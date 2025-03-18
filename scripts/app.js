7f13f19616591d91432f8e48520472ae1cb123a3
Khaled-kak <kaklipro@gmail.com>
2025-03-18 07:32:55 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-fetch('https://newsapi.org/v2/everything?q=Algérie&apiKey=TON_API_KEY')
+fetch('https://newsapi.org/v2/everything?q=Algérie&apiKey=fce4d310a188453eaa6c3118531b7226')
     .then(response => response.json())
     .then(data => {
         const articles = data.articles;
