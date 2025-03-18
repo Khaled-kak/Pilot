fa04283f82e8c6550cf43781a4f7fa9a00187474
Khaled-kak <kaklipro@gmail.com>
2025-03-18 07:32:09 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-
+fetch('https://newsapi.org/v2/everything?q=Algérie&apiKey=TON_API_KEY')
+    .then(response => response.json())
+    .then(data => {
+        const articles = data.articles;
+        // Traiter les articles et les afficher
+    });
